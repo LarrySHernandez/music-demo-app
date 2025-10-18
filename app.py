a1400052a788bb39e34c0d4783fc4e78d3c390af
Larry <larryh2612@gmail.com>
2025-10-18 16:53:40 -0400

    alpha 1.1

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
 app.debug = False
 
 @app.route('/')
